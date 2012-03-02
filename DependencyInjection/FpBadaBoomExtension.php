504ba9e342185b348c20967eabf2648834d8baba
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-03-02 16:39:16 +0200

    bundle skeleton

---
+++ DependencyInjection/FpBadaBoomExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Fp\BadaBoomBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class FpBadaBoomExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+
+    }
+}
---
+++ FpBadaBoomBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Fp\BadaBoomBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FpBadaBoomBundle extends Bundle
+{
+}
