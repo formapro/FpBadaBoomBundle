66f4785b1ce6602ee57e5a5f444cf7cfa7b22c8a
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-09-28 16:13:49 +0300

    impr custom catcher and manager init.

--- FpBadaBoomBundle.php
+++ FpBadaBoomBundle.php
@@ -21,16 +21,6 @@ class FpBadaBoomBundle extends Bundle
      * @var ChainNode\ChainNodeManagerInterface
      */
     protected $chainNodeManager;
-
-    /**
-     * @param ExceptionCatcher\ExceptionCatcherInterface $exceptionCatcher
-     * @param ChainNode\ChainNodeManagerInterface $chainNodeManager
-     */
-    public function __construct(ExceptionCatcherInterface $exceptionCatcher = null, ChainNodeManagerInterface $chainNodeManager = null)
-    {
-        $this->exceptionCatcher = $exceptionCatcher;
-        $this->chainNodeManager = $chainNodeManager;
-    }
     
     /**
      * {@inheritdoc}
@@ -38,12 +28,17 @@ class FpBadaBoomBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+
+        $badaBoomBundleDefinition = new Definition();
+        $badaBoomBundleDefinition->setClass(get_class($this));
+        $badaBoomBundleDefinition->setSynthetic(true);
+        $container->setDefinition('fp_badaboom', $badaBoomBundleDefinition);
         
         if ($this->exceptionCatcher) { 
             $exceptionCatcherDefinition = new Definition();
             $exceptionCatcherDefinition->setClass('Fp\BadaBoomBundle\BadaBoomFactory');
-            $exceptionCatcherDefinition->setFactoryService('fp_badaboom.factory');
-            $exceptionCatcherDefinition->setFactoryMethod('createExceptionCatcher');
+            $exceptionCatcherDefinition->setFactoryService('fp_badaboom');
+            $exceptionCatcherDefinition->setFactoryMethod('getExceptionCatcher');
             
             $container->setDefinition('fp_badaboom.exception_catcher', $exceptionCatcherDefinition);
         }
@@ -51,8 +46,8 @@ class FpBadaBoomBundle extends Bundle
         if ($this->chainNodeManager) {
             $chainNodeManagerDefinition = new Definition();
             $chainNodeManagerDefinition->setClass('Fp\BadaBoomBundle\BadaBoomFactory');
-            $chainNodeManagerDefinition->setFactoryService('fp_badaboom.factory');
-            $chainNodeManagerDefinition->setFactoryMethod('createChainNodeManager');
+            $chainNodeManagerDefinition->setFactoryService('fp_badaboom');
+            $chainNodeManagerDefinition->setFactoryMethod('getChainNodeManager');
 
             $container->setDefinition('fp_badaboom.chain_node_manager', $chainNodeManagerDefinition);
         }
@@ -60,18 +55,52 @@ class FpBadaBoomBundle extends Bundle
         $container->addCompilerPass(new AddSerializerNormalizersPass);
         $container->addCompilerPass(new AddSerializerEncodersPass);
     }
+
+    /**
+     * @return ExceptionCatcher\ExceptionCatcherInterface
+     */
+    public function getExceptionCatcher()
+    {
+        return $this->exceptionCatcher;
+    }
+
+    /**
+     * @return ChainNode\ChainNodeManagerInterface
+     */
+    public function getChainNodeManager()
+    {
+        return $this->chainNodeManager;
+    }
     
     public function boot()
     {
+        $this->container->set('fp_badaboom', $this);
+        
         /** @var $exceptionCatcher ExceptionCatcherInterface */
         $exceptionCatcher = $this->container->get('fp_badaboom.exception_catcher');
         /** @var $chainNodeManager ChainNodeManagerInterface */
         $chainNodeManager = $this->container->get('fp_badaboom.chain_node_manager');
 
-        $exceptionCatcher->start();
+        $exceptionCatcher->start($this->container->getParameter('kernel.debug'));
         
         foreach ($chainNodeManager->all() as $chainNode) {
             $exceptionCatcher->registerChainNode($chainNode);
         }
+    }
+
+    /**
+     * @param \Fp\BadaBoomBundle\ExceptionCatcher\ExceptionCatcherInterface $exceptionCatcher
+     */
+    public function setExceptionCatcher(ExceptionCatcherInterface $exceptionCatcher)
+    {
+        $this->exceptionCatcher = $exceptionCatcher;
+    }
+
+    /**
+     * @param \Fp\BadaBoomBundle\ChainNode\ChainNodeManagerInterface $chainNodeManager
+     */
+    public function setChainNodeManager(ChainNodeManagerInterface $chainNodeManager)
+    {
+        $this->chainNodeManager = $chainNodeManager;
     }
 }
