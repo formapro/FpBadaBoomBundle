8e7681cf114ffd12ef897868b245f52a059e4737
Michael Grinko <glukym@gmail.com>
2013-05-03 14:37:19 +0300

    JavascriptExceptionTest was added

---
+++ Tests/JsLogger/JavascriptExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Fp\BadaBoomBundle\Tests\JsLogger;
+
+use Fp\BadaBoomBundle\JsLogger\JavascriptException;
+
+class JavascriptExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithoutArguments()
+    {
+        new JavascriptException;
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeSubclassOfException()
+    {
+        $this->assertInstanceOf('Exception', new JavascriptException);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetCorrectInitializationValues()
+    {
+        $exception = new JavascriptException('test_message', 'test_file', 10);
+
+        $this->assertEquals('test_file', $exception->getFile());
+        $this->assertEquals(10, $exception->getLine());
+    }
+}
